cef32babd513ede1ffb277ac78c5f273c002a9cb
Jeii22 <jeikur42@gmail.com>
2025-10-09 18:31:11 +0800

    test gg7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Balt-Bep</title>
+    <title>Balt Bep</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
